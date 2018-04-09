902b1c8cf1a0690254d51613957e0134a34fb0eb
bonjourbonobo <37870337+bonjourbonobo@users.noreply.github.com>
2018-04-09 21:07:18 +0800

    Update 461. Hamming Distance.java

--- Java/461. Hamming Distance.java
+++ Java/461. Hamming Distance.java
@@ -42,3 +42,18 @@ class Solution {
         return result;
     }
 }
+
+// Solution 3
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int result = 0;
+        while(x>0 || y>0){
+            if(x%2 != y%2){
+                result++;
+            }
+            x /= 2;
+            y /= 2;
+        }
+        return result;
+    }
+}
