7d7f570347bf4ca9e16e54e20b60917704e199b1
bonjourbonobo <37870337+bonjourbonobo@users.noreply.github.com>
2019-11-30 08:07:56 +0800

    Create 70. Climbing Stairs.java

---
+++ Java/70. Climbing Stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+        map.put(1, 1);
+        map.put(2, 2);
+        for(int i=3; i<=n; i++){
+            map.put(i, map.get(i-1) + map.get(i-2));
+        }
+        return map.get(n);
+        
+    }
+}
